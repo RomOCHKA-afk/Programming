102d7186fc089330328543f7cc3cce9fd58a7111
RomOCHKA-afk <79787542024v@gmail.com>
2021-03-14 22:00:30 +0300

    entered 33-35

---
+++ Practice/29/C++/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	float a, b;
+	cin >> a >> b;
+	cout << (a+b) << endl;
+	return 0;
+}
---
+++ Practice/29/C++/ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int* ptrarr = new int[10];
+	delete[] ptrarr;
+
+	return 0;
+
+
+}
